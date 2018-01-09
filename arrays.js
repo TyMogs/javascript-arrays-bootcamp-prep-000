df47a1bb8ca6cc9cc3dd1ed6f6b91c36aeba84ec
Tyler Morris <tyler.a.morris72@gmail.com>
2018-01-09 02:00:41 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArrayd(array, element) {
+  array.unshift(element)
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  
+}
