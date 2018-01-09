5a20b137304187bc010ffad645ce98874cc08278
Tyler Morris <tyler.a.morris72@gmail.com>
2018-01-09 02:16:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArrayd(array, element) {
+function addElementToBeginningOfArray(array, element) {
   var newArrayBegin = [element, ...array]
   return array
 }
